f26b974333da5a15158bf1bbdec82ec813310acf
Joon Hwa Tan <joonhwa20@hotmail.com>
2017-09-01 00:58:42 +0800

    Setup framework code

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var budgetController = (function() {
+    
+    
+})();
+
+
+
+var UIController = (function() {
+    
+    
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var ctrlAddItem = function() {
+        
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+    
+})(budgetController, UIController);
